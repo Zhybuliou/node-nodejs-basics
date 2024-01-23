b48ef0a15f5f38f70ad26a396a360756aaa208c2
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-01-23 13:15:28 +0300

    feat: return base branch main

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,18 +1,5 @@
 const create = async () => {
-    const fs = require("fs")
-    const path = __dirname + '/files/fresh.txt';
-    const data = 'I am fresh and young';
-
-    fs.access(path, fs.constants.F_OK, (err) => {
-        if (!err) {
-            throw new Error('FS operation failed');
-        } else {
-            fs.writeFile(path, data, (err) => {
-                if (err) throw err;
-                console.log('File has been created');
-            });
-        }
-    });
+    // Write your code here 
 };
 
-create();
+await create();
