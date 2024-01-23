11cc84065b61b401d659381d7b7e668bcea8038b
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-01-23 19:58:03 +0300

    feat: solved task fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import * as fs from 'fs'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const list = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const dirFiles = __dirname + '/files';
+    if (!fs.existsSync(dirFiles)) {
+        throw new Error('FS operation failed');
+    }
+    fs.readdir(dirFiles, (err, files) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        } else {
+            console.log(files);
+        }
+    });
 };
 
 await list();
