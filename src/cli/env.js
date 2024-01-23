7f60382c569903ca902ab9662ff5f46b031e1047
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-01-23 20:39:10 +0300

    feat: solved task env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,6 @@
 const parseEnv = () => {
-    // Write your code here 
+    const keys = Object.keys(process.env).filter((el) => el.includes('RSS_'))
+    keys.forEach((el) => console.log(`${el}=${process.env[el]}`))
 };
 
 parseEnv();
