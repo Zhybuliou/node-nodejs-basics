bce06a53752ed950bbdf94a344e6a8a9b4d2343d
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-01-23 22:32:36 +0300

    feat: add solved task calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import * as fs from 'fs'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import crypto from 'crypto';
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const fileToCalculateHashFor = __dirname + '/files/fileToCalculateHashFor.txt';
+    const readStream = fs.createReadStream(fileToCalculateHashFor);
+    const hash = crypto.createHash('sha256');
+
+    readStream.on('data', (chunk) => {
+        console.log(hash.update(chunk).digest('hex'));
+      });
 };
 
 await calculateHash();
