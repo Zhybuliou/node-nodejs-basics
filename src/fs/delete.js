c9352a5b3d48c272d9f84361244b39c2d9bfcf31
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-01-23 19:50:09 +0300

    feat: add solved task delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import * as fs from 'fs'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const remove = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const fileToRemove = __dirname + '/files/fileToRemove.txt';
+    if (!fs.existsSync(fileToRemove)) {
+        throw new Error('FS operation failed');
+    }
+    fs.unlinkSync(fileToRemove)
 };
 
 await remove();
