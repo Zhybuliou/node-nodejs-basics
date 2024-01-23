6f65c73060abdb2037e072e165137716daefa224
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-01-23 19:40:40 +0300

    feat: solved task fs:rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,28 @@
+import * as fs from 'fs'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const wrongFilename = __dirname + '/files/wrongFilename.txt';
+    const properFilename = __dirname + '/files/properFilename.md';
+
+    if (!fs.existsSync(wrongFilename)) {
+        throw new Error('FS operation failed');
+    }
+    if (fs.existsSync(properFilename)) {
+        throw new Error('FS operation failed');
+    }
+    fs.rename(wrongFilename, properFilename, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    });
 };
 
 await rename();
+
+// rename.js - implement function that renames file wrongFilename.txt 
+// to properFilename with extension .md 
+// (if there's no file wrongFilename.txt or properFilename.md already exists Error with message FS 
+// operation failed must be thrown)
