7f2d4afe7034946fdfdac58c69dfa02b8db09b60
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-01-23 13:01:33 +0300

    feat: add solution task fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
 const create = async () => {
-    // Write your code here 
+    const fs = require("fs")
+    const path = __dirname + '/files/fresh.txt';
+    const data = 'I am fresh and young';
+
+    fs.access(path, fs.constants.F_OK, (err) => {
+        if (!err) {
+            throw new Error('FS operation failed');
+        } else {
+            fs.writeFile(path, data, (err) => {
+                if (err) throw err;
+                console.log('File has been created');
+            });
+        }
+    });
 };
 
-await create();
+create();
