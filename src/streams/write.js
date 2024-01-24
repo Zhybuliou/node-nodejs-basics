8673027c81dd9c279094120b909805bcd0831153
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-01-24 19:24:32 +0300

    feat: solved task streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import * as fs from 'fs'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 const write = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const file = __dirname + '/files/fileToWrite.txt';
+    const writeStream = fs.createWriteStream(file);
+    process.stdin.on('data', (chunk) => writeStream.write(chunk));
+
 };
 
 await write();
