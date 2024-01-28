e6b3349f1c680ff2c5778157e8900b8c0154399b
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-01-28 23:12:10 +0300

    feat: add solved task child_process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,28 @@
+import { spawn } from 'child_process';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const scriptFile = __dirname + '/files/script.js';
+    const childProcess = spawn('node', [scriptFile, args], { stdio: 'pipe' });
+
+  childProcess.stdout.on('data', (data) => {
+    console.log(`Received from child process: ${data.toString()}`);
+  });
+
+  childProcess.stderr.on('data', (data) => {
+    console.error(`Error from child process: ${data.toString()}`);
+  });
+
+  process.stdin.pipe(childProcess.stdin);
+
+  process.stdin.on('data', (data) => {
+    if (data.toString().includes('CLOSE')) {
+      childProcess.kill();
+      process.exit(0);
+    }
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['1', '2']);
